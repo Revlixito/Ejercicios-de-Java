dfad6a4254f9089e3309d7b6763362ed6e2cfd82
Iker <ikerdlf0106@gmail.com>
2025-11-21 00:31:18 +0100

    Completados los ejercicios 1 2 3 del Tema 1.

--- Unit01/src/unit1/Actividad02/Actividad2.java
+++ Unit01/src/unit1/Actividad02/Actividad2.java
@@ -6,13 +6,28 @@ import java.util.Scanner;
     Diseñar un programa que pida un número al usuario (por teclado) y a continuación
     lo muestre por pantalla.
  */
+// public class Actividad2 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int num;
+//         System.out.print("Introduce un número: ");
+//         num = sc.nextInt();
+//         System.out.println("El número introducido es: " + num);
+
+//     }
+// }
+
 public class Actividad2 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num;
-        System.out.print("Introduce un número: ");
-        num = sc.nextInt();
-        System.out.println("El número introducido es: " + num);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Porfavor introduce un numero");
+
+        int numero = scan.nextInt();
+        scan.close();
+
+        System.out.println("Tu numero es el " + numero);
 
     }
 }
+
--- Unit01/src/unit1/Actividad03/Actividad3.java
+++ Unit01/src/unit1/Actividad03/Actividad3.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 /*
     Pedir al usuario su edad y mostrar la que tendrá el próximo año.
  */
+// public class Actividad3 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int edad;
+//         System.out.print("Introduce tu edad: ");
+//         edad = sc.nextInt();
+//         System.out.println("El año que viene tendrás " + (edad + 1) + " años.");
+//     }
+// }
+
+
 public class Actividad3 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int edad;
-        System.out.print("Introduce tu edad: ");
-        edad = sc.nextInt();
-        System.out.println("El año que viene tendrás " + (edad + 1) + " años.");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Introduce tu edad");
+
+        int edad = scan.nextInt();
+        scan.close();
+        System.out.println("Tu edad el año que viene sera " + (edad+1));
+
     }
 }
